7a49dea5f436f3f6ec48fac4a73474be19a7282f
Hieu.Le <letrunghieu2602@gmail.com>
2020-03-31 17:47:25 +0700

    đặt lại theo OOP

--- BallandMove.js
+++ BallandMove.js
@@ -1,12 +1,17 @@
 let canvas = document.getElementById('myGame');
 let context = canvas.getContext('2d');
 
-let x = 7; y = 7;
-let dx = 3; dy = 3;
+let thamsoBall = {
+    x: 7,
+    y: 7,
+    radius: 7,
+    dx: 3,
+    dy: 3,
+};
 //vẽ quả bóng
 let drawBall = function () {
     context.beginPath();
-    context.arc(x,y,7,0,Math.PI*2);
+    context.arc(thamsoBall.x, thamsoBall.y,thamsoBall.radius,0,Math.PI*2);
     context.fill();
     context.closePath();
 };
@@ -14,14 +19,15 @@ let drawBall = function () {
 function moveBall() {
     context.clearRect(0,0,canvas.width,canvas.height);
     drawBall();
-    if (y<0 || y>canvas.height) {
+    if (y<7 || y>canvas.height-7) {
         dy = -dy;
     }
-    if (x<0 || x>canvas.width) {
+    if (x<7 || x>canvas.width-7) {
         dx = -dx;
     }
     x += dx;
     y += dy;
     requestAnimationFrame(moveBall);
 }
-moveBall();
+// moveBall();
+drawBall();
