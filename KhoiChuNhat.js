122bac1dfe9f2e1ddcc66bdc5e1da6c763c3a90b
Hieu.Le <letrunghieu2602@gmail.com>
2020-03-31 17:47:47 +0700

    Đặt lại theo OOP

--- KhoiChuNhat.js
+++ KhoiChuNhat.js
@@ -1,18 +1,34 @@
-let drawHCN = function () {
-
-    for (i=0;i<7;i++) {
-        for (j=0;j<12;j++) {
-            context.beginPath();
-            context.rect(
-            15 + j*(45+20),
-            40 + i*(15+15),
-            45,
-            15);
-            context.fillStyle = 'green';
-            context.fill();
-            context.closePath();
+let thamsoHCN = {
+    weight: 40,
+    height: 15,
+    marginLeft: 15,
+    marginTop: 30,
+    marginBetween: 20};
+let khoiHCN = function() {
+    this.x = thamsoHCN.marginLeft;
+    this.y = thamsoHCN.marginTop;
+    this.weight = thamsoHCN.weight;
+    this.height = thamsoHCN.height;
+    this.between = thamsoHCN.marginBetween;
+    this.draw = function () {
+        for (i=0;i<7;i++) {
+            for (j=0;j<13;j++) {
+                context.beginPath();
+                context.rect(
+                    this.x + j*(this.weight+this.between),
+                    this.y + i*(this.height+this.between),
+                    this.weight,
+                    this.height);
+                context.fillStyle = 'green';
+                context.fill();
+                context.closePath();
+            }
         }
     }
 };
-drawHCN();
+let HCN = new khoiHCN(thamsoHCN);
+HCN.draw();
+
+
+
 
