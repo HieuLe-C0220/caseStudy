60bfb96ccb43bd01b1674095bf442ceb192ed748
Hieu.Le <letrunghieu2602@gmail.com>
2020-04-02 00:43:23 +0700

    tạo riêng file để chạy game

---
+++ runGame.js
@@ -0,0 +1,27 @@
+
+let ball = new Ball(7,7,7,3,3);
+let gach = new HCN(15,30,40,15);
+let thanhDo = new ThanhDoBall();
+//vẽ các khối gạch
+
+//điều khiển bằng phím
+function keyDown(event) {
+    switch (event.keyCode) {
+        case 37:
+            thanhDo.moveLeft();
+            break;
+        case 39:
+            thanhDo.moveRight();
+            break;
+    }
+}
+document.addEventListener('keydown',keyDown);
+function drawBall() {
+    ball.moveBall();
+}
+
+function loadGame() {
+    drawBall();
+    thanhDo.drawThanh();
+    requestAnimationFrame(loadGame);
+}
