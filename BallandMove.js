e3c65e7a353bce65954320b7afd387b288ba99da
Hieu.Le <letrunghieu2602@gmail.com>
2020-04-02 00:42:01 +0700

    chỉnh sửa 1 số thuộc tính của bóng

--- BallandMove.js
+++ BallandMove.js
@@ -1,33 +1,32 @@
-let canvas = document.getElementById('myGame');
-let context = canvas.getContext('2d');
-
-let thamsoBall = {
-    x: 7,
-    y: 7,
-    radius: 7,
-    dx: 3,
-    dy: 3,
-};
-//vẽ quả bóng
-let drawBall = function () {
-    context.beginPath();
-    context.arc(thamsoBall.x, thamsoBall.y,thamsoBall.radius,0,Math.PI*2);
-    context.fill();
-    context.closePath();
-};
-//vẽ quả bóng chuyển động
-function moveBall() {
-    context.clearRect(0,0,canvas.width,canvas.height);
-    drawBall();
-    if (y<7 || y>canvas.height-7) {
-        dy = -dy;
+//tạo lớp quả bóng
+let Ball = function (x,y,radius,dx,dy) {
+    this.x = x;
+    this.y = y;
+    this.radius = radius;
+    this.dx = dx; //Gia tốc theo phương x
+    this.dy = dy; //Gia tốc theo phương y
+    this.draw = function() { //vẽ quả bóng
+        context.beginPath();
+        context.arc(this.x, this.y,this.radius,0,Math.PI*2);
+        context.fillStyle = 'black';
+        context.fill();
+        context.closePath();
+    };
+    // this.setBall = function () {
+    //     this.x = this.x +this.dx;
+    //     this.y = this.y + this.dy;
+    // };
+        let seft = this;
+    this.moveBall = function () { //vẽ quả bóng chuyển động
+        this.x += this.dx;
+        this.y += this.dy;
+        context.clearRect(0,0,canvas.width,canvas.height);
+        seft.draw();
+        if (this.y<this.radius || this.y>canvas.height-this.radius) {
+            this.dy = -this.dy;
+        }
+        if (this.x<this.radius || this.x>canvas.width-this.radius) {
+            this.dx = -this.dx;
+        }
     }
-    if (x<7 || x>canvas.width-7) {
-        dx = -dx;
-    }
-    x += dx;
-    y += dy;
-    requestAnimationFrame(moveBall);
-}
-// moveBall();
-drawBall();
+};
