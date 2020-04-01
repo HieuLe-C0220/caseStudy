f66f710b387da62c73267fb8e1985e3bbcf5d34c
Hieu.Le <letrunghieu2602@gmail.com>
2020-04-02 00:43:03 +0700

    sửa lại theo OOP

--- ThanhDoBall.js
+++ ThanhDoBall.js
@@ -3,16 +3,35 @@ let thamsoThanh = {
     marginTop: canvas.height - 10,
     weight: 70,
     height: 10,
+    speed: 15,
 };
 let ThanhDoBall = function () {
-    context.beginPath();
-    context.rect(
-        thamsoThanh.marginLeft,
-        thamsoThanh.marginTop,
-        thamsoThanh.weight,
-        thamsoThanh.height);
-    context.fillStyle = 'gray';
-    context.fill();
-    context.closePath();
+    this.x = thamsoThanh.marginLeft;
+    this.y = thamsoThanh.marginTop;
+    this.w = thamsoThanh.weight;
+    this.h = thamsoThanh.height;
+    this.speed = thamsoThanh.speed;
+    this.isLeft = false;
+    this.isRight = false;
+    this.drawThanh = function() { //vẽ thanh đỡ
+        context.beginPath();
+        context.rect(this.x, this.y, this.w, this.h);
+        context.fillStyle = 'gray';
+        context.fill();
+        context.closePath();
+    };
+    this.moveLeft = function () { //thanh di chuyển sang trái
+        this.x -= this.speed;
+        if (this.x<0) {
+            this.x = 0;
+        }
+    };
+    this.moveRight = function () { //thanh di chuyen sang phải
+        this.x += this.speed;
+        if (this.x> canvas.width - this.w) {
+            this.x= canvas.width - this.w;
+        }
+    };
 };
-ThanhDoBall();
+
+
