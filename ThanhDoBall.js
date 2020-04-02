da0087e716bb917f2c9b25d3cea6ca5530773d1e
Hieu.Le <letrunghieu2602@gmail.com>
2020-04-02 23:20:22 +0700

    edit toạ độ thanh và thêm property đỡ bóng

--- ThanhDoBall.js
+++ ThanhDoBall.js
@@ -1,15 +1,14 @@
 let thamsoThanh = {
-    marginLeft: 0,
-    marginTop: canvas.height - 10,
-    weight: 70,
-    height: 10,
-    speed: 15,
+    marginTop: 30,
+    width: 70,
+    height: 12,
+    speed: 25,
 };
 let ThanhDoBall = function () {
-    this.x = thamsoThanh.marginLeft;
-    this.y = thamsoThanh.marginTop;
-    this.w = thamsoThanh.weight;
+    this.w = thamsoThanh.width;
     this.h = thamsoThanh.height;
+    this.x = canvas.width/3-this.w/2;
+    this.y = canvas.height - this.h - thamsoThanh.marginTop;
     this.speed = thamsoThanh.speed;
     this.isLeft = false;
     this.isRight = false;
@@ -32,6 +31,12 @@ let ThanhDoBall = function () {
             this.x= canvas.width - this.w;
         }
     };
+    this.doBall = function (ball) {
+        if (ball.x + ball.radius > this.x && ball.x + ball.radius < this.x + this.w
+            && ball.y + ball.radius > this.y && ball.y - ball.radius < this.y + this.h) {
+            ball.dy = -ball.dy;
+        }
+    }
 };
 
 
