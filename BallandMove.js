3d8412d908e58394d2fee17b0ffae4b800204f5e
Hieu.Le <letrunghieu2602@gmail.com>
2020-04-02 23:21:49 +0700

    sửa lại toạ độ ban đầu của bóng

--- BallandMove.js
+++ BallandMove.js
@@ -1,8 +1,8 @@
 //tạo lớp quả bóng
-let Ball = function (x,y,radius,dx,dy) {
-    this.x = x;
-    this.y = y;
+let Ball = function (radius,dx,dy) {
+    this.x = canvas.width/3;
     this.radius = radius;
+    this.y = canvas.height-this.radius-42;
     this.dx = dx; //Gia tốc theo phương x
     this.dy = dy; //Gia tốc theo phương y
     this.draw = function() { //vẽ quả bóng
@@ -16,16 +16,13 @@ let Ball = function (x,y,radius,dx,dy) {
     //     this.x = this.x +this.dx;
     //     this.y = this.y + this.dy;
     // };
-        let seft = this;
     this.moveBall = function () { //vẽ quả bóng chuyển động
         this.x += this.dx;
         this.y += this.dy;
-        context.clearRect(0,0,canvas.width,canvas.height);
-        seft.draw();
-        if (this.y<this.radius || this.y>canvas.height-this.radius) {
+        if (this.y < this.radius) {
             this.dy = -this.dy;
         }
-        if (this.x<this.radius || this.x>canvas.width-this.radius) {
+        if (this.x < this.radius || this.x>canvas.width-this.radius) {
             this.dx = -this.dx;
         }
     }
